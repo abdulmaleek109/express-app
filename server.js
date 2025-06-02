1a4a20c88a3323b4992e546501e1f746e3e37f77
D2500683 <dihemegbulem@gmail.com>
2025-06-02 13:47:25 +0400

    added the put method

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
     next();
 });
 
+
 app.use('/images', (req, res, next) => {
     const imagePath = path.join(__dirname, 'public/images', req.path);
 
@@ -42,4 +43,61 @@ app.use('/images', (req, res, next) => {
         }
         next()
     })
+});
+
+
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+let db
+
+async function connectToDatabase(){
+    try{
+        await client.connect();
+        db = client.db('lessonshop'); 
+        console.log('Connected to Mongo db atlas');
+    }catch(error){
+        console.error('MongoDB connection error:', error)
+    }
+}
+async function startServer() {
+    try {
+        await client.connect();
+        db = client.db('lessonshop');
+        console.log('Connected to Mongo db atlas');
+
+        app.get('/api/lessons', async (req, res) => {
+    try{
+        const lessons = await db.collection('lessons').find({}).toArray();
+        res.json(lessons);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
+
+app.put('/api/lessons/:id', async (req, res) => {
+    try {
+        const lessonId = req.params.id;
+
+        if (!ObjectId.isValid(lessonId)) {
+            return res.status(400).json({ message: "Invalid ID format" });
+        }
+        const result = await db.collection('lessons').updateOne(
+            { _id: new ObjectId(lessonId) },
+            { $set: req.body }
+        );
+
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ message: "Lesson not found" });
+        }
+        const updatedLesson = await db.collection('lessons').findOne(
+            { _id: new ObjectId(lessonId) }
+        );
+
+        res.status(200).json(updatedLesson);
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error: error.message });
+    }
 });
